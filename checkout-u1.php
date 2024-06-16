df1af2f6a4f61f33e11578244f9769f9741829be
Hassan <hassanabbas4745@gmail.com>
2024-06-16 15:09:44 +0500

    wip: get all the products on the checkout page

--- checkout-u1.php
+++ checkout-u1.php
@@ -2,10 +2,26 @@
 session_start();
 
 require_once __DIR__ . '/utils/functions.php';
+require_once __DIR__ . '/utils/db.php';
 
 $ageRange = $_SESSION['AGE_RANGE'] ?? '';
 $overWeightReason = $_SESSION['OVERWEIGHT_REASON'] ?? '';
 $desiredWeight = $_SESSION['summary']['desired_weight'] ?? '';
+
+// Getting all the products
+$conn = getDbConnection();
+
+$sql = "SELECT * FROM products";
+$result = $conn->query($sql);
+
+$products = [];
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $products[] = $row;
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
