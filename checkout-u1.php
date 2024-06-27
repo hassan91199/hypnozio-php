b7b9581eaa3052001c3b7d63db3d6f287fc96d8b
Hassan <hassanabbas4745@gmail.com>
2024-06-28 00:11:09 +0500

    fix: make the payment modal go fullscreen on xs screens

--- checkout-u1.php
+++ checkout-u1.php
@@ -496,7 +496,7 @@ foreach ($products as $key => $product) {
                                         <img src="assets/images/safe-checkout-en.png" alt="" />
                                     </div>
                                     <div x-cloak x-show="showModal" class="fixed left-0 top-0 w-full h-full flex justify-center items-center bg-modal z-10">
-                                        <div x-on:click.outside="showModal = false; event.preventDefault(); disableCta = false" class="bg-white p-6 rounded">
+                                        <div x-on:click.outside="showModal = false; event.preventDefault(); disableCta = false" class="xs:h-full bg-white p-6 rounded">
                                             <a class="close-btn" href="/checkout-u1.php?d=70" @click="showModal = false; disableCta = false">&times;</a>
                                             <div class="text-center">
                                                 <h2 class="text-headline-small md:text-headline-medium font-bold">
